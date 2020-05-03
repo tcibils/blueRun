6f5ff7377c805acd183677d8772bf3bbb3ae2b12
Thomas Cibils <thomas.cibils@gmail.com>
2020-05-03 16:32:47 +0200

    Separating player moving and data display functions

--- Main/Main.ino
+++ Main/Main.ino
@@ -122,64 +122,7 @@ if(lastMillis - millis() > 500) {
     }
     lastDownButtonValue = downButtonValue; // And we update what we read just after
 
+  updateLEDMatrix();
   outputDisplay();
   delay(1);
-}
-
-// Makes the whole "LEDMatrix" equals to 0, i.e. each LED is off
-void clearLEDMatrix() {
-  // Just seting le LEDmatrix to Wall
-  for (byte i = 0; i < displayNumberOfRows; i++)  {
-    for (byte j = 0; j < displayNumberOfColumns; j++) {
-      LEDMatrix[i][j] = Black;
-    }
-  }
-}
-
-// We update the physical display of the LED matrix, based on the LEDMatrix
-void outputDisplay() {
-  for(byte rowIndex = 0; rowIndex < displayNumberOfRows; rowIndex++) {
-    for(byte columnIndex = 0; columnIndex < displayNumberOfColumns; columnIndex++) {
-      // Useful because of the way my matrix is soldered.
-      // So we'll invert one column every two compared to our digital matrix
-      // If we're on an even column, we're fine, everything is straightfoward
-      if(columnIndex%2 == 0) {
-        
-        if(LEDMatrix[rowIndex][columnIndex] == Black) {leds[(columnIndex + 1)*displayNumberOfRows - rowIndex - 1] = CRGB::Black;}
-        if(LEDMatrix[rowIndex][columnIndex] == White) {leds[(columnIndex + 1)*displayNumberOfRows - rowIndex - 1] = CRGB::White;}
-        if(LEDMatrix[rowIndex][columnIndex] == Green) {leds[(columnIndex + 1)*displayNumberOfRows - rowIndex - 1] = CRGB::Green;}
-        if(LEDMatrix[rowIndex][columnIndex] == Blue) {leds[(columnIndex + 1)*displayNumberOfRows - rowIndex - 1] = CRGB::Blue;}
-        if(LEDMatrix[rowIndex][columnIndex] == Red) {leds[(columnIndex + 1)*displayNumberOfRows - rowIndex - 1] = CRGB::Red;}
-        if(LEDMatrix[rowIndex][columnIndex] == Purple) {leds[(columnIndex + 1)*displayNumberOfRows - rowIndex - 1] = CRGB::Purple;}
-      }
-      // If we're on an uneven column, we do a mathematical trick to invert it
-      else if(columnIndex%2 == 1) {
-        if(LEDMatrix[rowIndex][columnIndex] == Black) {leds[columnIndex*displayNumberOfRows + rowIndex] = CRGB::Black;}
-        if(LEDMatrix[rowIndex][columnIndex] == White) {leds[columnIndex*displayNumberOfRows + rowIndex] = CRGB::White;}
-        if(LEDMatrix[rowIndex][columnIndex] == Green) {leds[columnIndex*displayNumberOfRows + rowIndex] = CRGB::Green;}
-        if(LEDMatrix[rowIndex][columnIndex] == Blue) {leds[columnIndex*displayNumberOfRows + rowIndex] = CRGB::Blue;}
-        if(LEDMatrix[rowIndex][columnIndex] == Red) {leds[columnIndex*displayNumberOfRows + rowIndex] = CRGB::Red;}
-        if(LEDMatrix[rowIndex][columnIndex] == Purple) {leds[columnIndex*displayNumberOfRows + rowIndex] = CRGB::Purple;}
-      }
-    }
-  }
-  
-  // Display the matrix physically
-  FastLED.show(); 
-}
-
-// We update the digital display of the LED matrix
-void digitalOutputDisplay() {
-  Serial.print("\n We print digitally the current theoritical state of the LED Matrix : \n");
-  for (byte i = 0; i < displayNumberOfRows; i++) {
-    for (byte j = 0; j < displayNumberOfColumns; j++) {
-      Serial.print(LEDMatrix[i][j]);
-      if (j < displayNumberOfColumns - 1) {
-        Serial.print(", ");
-      }
-      else {
-        Serial.print("\n");
-      }
-    }
-  }
 }
--- Main/playerFunctions.ino
+++ Main/playerFunctions.ino
@@ -1,46 +1,33 @@
-void setPlayerPositionInMatrix() {
-  LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Blue;
-}
-
-
 // ----------- Moving Functions -----------------
 
 void movePlayerLeft() {
   // We don't want the player getting outside of the frame
   if(playerPosition.columnCoordinate > 0){
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Black;
     byte temp = playerPosition.columnCoordinate;
     playerPosition.columnCoordinate = temp - 1;    
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Blue;
   }
 }
 
 void movePlayerRight() {
   // We don't want the player getting outside of the frame
   if(playerPosition.columnCoordinate < displayNumberOfColumns-1){
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Black;
     byte temp = playerPosition.columnCoordinate;
     playerPosition.columnCoordinate = temp + 1;
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Blue; 
   }
 }
 
 void movePlayerUp() {
   // We don't want the player getting outside of the frame
   if(playerPosition.lineCoordinate > 0){
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Black;
     byte temp = playerPosition.lineCoordinate;
     playerPosition.lineCoordinate = temp - 1;  
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Blue;  
   }
 }
 
 void movePlayerDown() {
   // We don't want the player getting outside of the frame
   if(playerPosition.lineCoordinate < displayNumberOfRows   - 1){
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Black;
     byte temp = playerPosition.lineCoordinate;
     playerPosition.lineCoordinate = temp + 1;    
-    LEDMatrix[playerPosition.lineCoordinate][playerPosition.columnCoordinate] = Blue;
   }
 }
