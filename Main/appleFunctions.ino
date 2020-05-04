dbc0fd6c7bfc5a460f259cf76ff9b7d207a12fe8
Thomas Cibils <thomas.cibils@gmail.com>
2020-05-04 22:03:41 +0200

    Generates the apple not near the player

--- Main/appleFunctions.ino
+++ Main/appleFunctions.ino
@@ -1,8 +1,20 @@
 void appleGeneration() {
-  randomSeed(analogRead(0));
-  byte rowPosition = random(displayNumberOfRows);
-  byte columnPosition = random(displayNumberOfColumns);
+  randomSeed(analogRead(A0));
 
+  bool appleFarFromPlayer = false;
+
+  // While the apple is seeded too near to the player, we re-generate one.
+  while(!appleFarFromPlayer) {
+    byte rowPosition = random(displayNumberOfRows);
+    byte columnPosition = random(displayNumberOfColumns);
+    if(rowPosition > playerPosition.lineCoordinate || playerPosition.lineCoordinate < rowPosition) {
+      if(columnPosition < playerPosition.columCoordinate || playerPosition.columCoordinate > playerPosition) {
+        appleFarFromPlayer = true;
+      }
+    } 
+   }
+   
+  /*
   Serial.print("rowPosition: ");
   Serial.print(rowPosition);
   Serial.print("\n");
@@ -11,6 +23,7 @@ void appleGeneration() {
   Serial.print(columnPosition);
   Serial.print("\n");
   Serial.print("\n");
+  */
   
   applePosition = {rowPosition, columnPosition};
   applePresence = 1;
